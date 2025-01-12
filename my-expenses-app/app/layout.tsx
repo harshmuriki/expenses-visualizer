c89a9ba7ed9ca1f6fbbb5059e0f8563976daedc2
harshmuriki <harshsuhith@gmail.com>
2025-01-11 20:01:34 -0500

    Integrate Vercel Analytics into the layout and update Vercel configuration for API routing

--- my-expenses-app/app/layout.tsx
+++ my-expenses-app/app/layout.tsx
@@ -3,6 +3,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          {children}
+          <Analytics />
+        </SessionProvider>
       </body>
     </html>
   );
